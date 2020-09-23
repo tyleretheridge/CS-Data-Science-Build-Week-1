0afcafd25f0eac39b44d2ae686f3b417e4d54844
tyleretheridge <tylerjetheridge98@gmail.com>
2020-09-22 19:20:10 -0500

    Draft class template for algorithm

---
+++ nearestneighbor.py
@@ -0,0 +1,18 @@
+# nearestneighbor.py
+import numpy as np
+
+# Implement the K-Nearest Neighbors algorithm as a Python Class
+# Required Methods: Fit, Predict
+# Implement a form of KNN that works for continuous data via a method based
+# on Euclidean distance
+
+class NearestNeighbor:
+    def __init__(self):
+        pass
+
+    def fit(self, parameter_list):
+        pass
+    
+    def predict(self, parameter_list):
+        pass
+
