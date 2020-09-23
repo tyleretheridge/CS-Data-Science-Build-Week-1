728d3276d644ecfd3d52f557731764ba3d959f60
tyleretheridge <tylerjetheridge98@gmail.com>
2020-09-23 04:55:19 -0500

    Add counting algorithm

--- nearestneighbor.py
+++ nearestneighbor.py
@@ -13,13 +13,14 @@ class NearestNeighbor:
     """
     # Init is where user will define parameters for usage
     def __init__(self, n_neighbors=5, X_train=None, y_train=None):
-        self.n_neighbors = n_neighbors
+        self.k = n_neighbors
 
-    # Private methods that will be used for algorithm calculations
-    def __distance(self, vec_a, vec_b):
+    # Methods that will be used for algorithm calculations
+    def calc_distance(self, vec_a, vec_b):
         # TODO: Figure out how to deal with class labels in the matrices
         # Avoid performing distance on the class label if numeric
-        """Calculates and returns the distance between two vectors of various dimension
+        """Calculates and returns the distance between two vectors 
+        of various dimensions.
 
         Args:
             vec_a ([type]): [description]
@@ -30,32 +31,81 @@ class NearestNeighbor:
         """
         return np.linalg.norm(vec_a - vec_b)
         
-    def __neighborscalc(self, parameter_list):
-        """[summary]
+    def calc_neighbors(self, x):
+        """Returns a list of distances between the parameter "x",
+        and the entries in the training data.
 
         Args:
-            parameter_list ([type]): [description]
+            x (1-D array): Test point to be classified
         """
-        # Make an empty list
-
-        # Use self.__distance() to calculate distances for every relation
-
-        # Append results of distance calc to list
-
-
-        # Return a list of distances between a record and all of its neighbor
-        # relationships
-        return
-
+        distance_list = list()
+        # For every entry in X_train
+        for x_train in self.X_train:
+            # Calculate distance between x (parameter) and each entry
+            distance_list.append(self.calc_distance(x,x_train)) 
+        
+        # A list of length self.k of the indices of the closest points to
+        # input x. 
+        k_index = np.argsort(distance_list)[:self.k]
 
 
+    def calc_most_common_label(self, arr):
+        """A counting algorithm that will return the most common 
+        class label in the passed array.
+
+        Args:
+            arr ([type]): An array of k_indices
+        """
+        # Get the maximum number of class labels
+        maximum = len(set(self.y_train))
+        # list comprehension to create a buckets array
+        buckets = [0 for i in range(maximum + 1)]
+        # Count the labels and increment the buckets
+        for value in arr:
+            buckets[value] += 1
+        # Return the index of the largest bucket
+        # AKA return the label with the highest occurrence in the list
+        most_common_label = buckets.index(max(buckets))
+        return most_common_label
+
+
+
+# Compare passed in parameter to every value in X_train
+    # Take in an X
+    # Use distance calc comparing it to X_train points
+    # Find a cutoff of nearest neighbors
+    # Take majority classification labels of the k nearest neighbors
+    # use np.argsort to return an array of sorted indices
+    # argsort returns a copy of the indices as if the original matrix were sorted
+    # useful for maintaining index integrity to label vector
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     # Class methods to be used by the end user, aka public methods
     def fit(self, X, y):
-        """[summary]
+        """Takes in training data and stores it for comparison.
 
         Args:
-            X ([type]): [description]
-            y ([type]): [description]
+            X ([type]): Feature matrix
+            y ([type]): Target vector
         """
         self.X_train = X
         self.y_train = y
